da67dff1f72b66a3ef6ad28fa0c12c5f0f59f04a
dotbig <ck.big@hotmail.com>
2019-03-06 18:49:00 +1300

    First commit

---
+++ app/src/main/java/dotbig/barebonesblackjack/GameActivity.java
@@ -0,0 +1,31 @@
+package dotbig.barebonesblackjack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        configureReturnButton();
+
+
+
+    }
+
+    private void configureReturnButton() {
+        Button returnButton = findViewById(R.id.buttonReturnToMain);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/dotbig/barebonesblackjack/MainActivity.java
@@ -0,0 +1,31 @@
+package dotbig.barebonesblackjack;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        configurePlayButton();
+
+    }
+
+    private void configurePlayButton() {
+        Button playButton = findViewById(R.id.buttonPlay);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+            }
+        });
+    }
+}
