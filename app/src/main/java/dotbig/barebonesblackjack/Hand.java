27e3ea1d3d514aae23b08d755a64716319fccc4e
dotbig <ck.big@hotmail.com>
2019-03-11 14:09:46 +1300

    fleshed out hand interface

--- app/src/main/java/dotbig/barebonesblackjack/Hand.java
+++ app/src/main/java/dotbig/barebonesblackjack/Hand.java
@@ -1,4 +1,10 @@
 package dotbig.barebonesblackjack;
 
 interface Hand {
+    Card hit();
+    Card hit(boolean faceUp);
+    int value();
+    boolean softSeventeen();
+    void clear();
+    String toString();
 }
