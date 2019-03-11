31bf26f83d6ea5a5e85b0e19c455b96ebfaaea8d
dotbig <ck.big@hotmail.com>
2019-03-11 14:29:18 +1300

    implement hit()

--- app/src/main/java/dotbig/barebonesblackjack/PlayHand.java
+++ app/src/main/java/dotbig/barebonesblackjack/PlayHand.java
@@ -1,4 +1,46 @@
 package dotbig.barebonesblackjack;
 
-public class PlayHand implements Hand {
+import java.util.List;
+import java.util.ArrayList;
+
+class PlayHand implements Hand {
+    List<Card> cards;
+    int bet;
+
+    PlayHand(){
+        cards = new ArrayList<>();
+    }
+
+    PlayHand(int stake){
+        cards = new ArrayList<>();
+        bet = stake;
+    }
+
+    public void hit(Card draw){
+        cards.add(draw);
+    }
+
+    public void hit(Card draw, boolean faceUp){
+        Card toAdd = draw;
+        if (toAdd.isFaceUp() != faceUp){
+            toAdd.flip(faceUp);
+        }
+        cards.add(draw);
+    }
+
+    public int value(){
+        return -1;
+    }
+
+    public boolean softSeventeen(){
+        return false;
+    }
+
+    public void clear(){
+        cards.clear();
+    }
+
+    public String toString(){
+        return "";
+    }
 }
