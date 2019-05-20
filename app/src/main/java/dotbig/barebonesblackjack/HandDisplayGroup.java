a1a5a1fa173103f045826cdefeccd8eabc5e479f
dotbig <ck.big@hotmail.com>
2019-05-20 12:53:23 +1200

    betting box reference
    HandDisplayGroup now holds a reference to its corresponding betting box so it's easier to adjust visibility of everything at once

--- app/src/main/java/dotbig/barebonesblackjack/HandDisplayGroup.java
+++ app/src/main/java/dotbig/barebonesblackjack/HandDisplayGroup.java
@@ -5,12 +5,15 @@ import android.widget.TextView;
 
 public class HandDisplayGroup implements AlphaSettable, VisibilitySettable, TextSettable {
     private ConstraintLayout cardDisplay;
+    private ConstraintLayout boxDisplay;
     private TextView betDisplay;
 
-    HandDisplayGroup(ConstraintLayout cardDisplay, TextView betDisplay){
+    HandDisplayGroup(ConstraintLayout cardDisplay, ConstraintLayout boxDisplay, TextView betDisplay){
         this.cardDisplay = cardDisplay;
+        this.boxDisplay = boxDisplay;
         this.betDisplay = betDisplay;
     }
+
 
     ConstraintLayout getCardDisplay(){
         return cardDisplay;
@@ -26,11 +29,13 @@ public class HandDisplayGroup implements AlphaSettable, VisibilitySettable, Text
 
     public void setAlpha(float alpha){
         cardDisplay.animate().alpha(alpha);
+        boxDisplay.animate().alpha(alpha);
         betDisplay.animate().alpha(alpha);
     }
 
     public void setVisibility(int visibility){
         cardDisplay.setVisibility(visibility);
+        boxDisplay.setVisibility(visibility);
         betDisplay.setVisibility(visibility);
     }
 
